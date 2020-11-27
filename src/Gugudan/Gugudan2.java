75bd66440fc733da395f511a047e61583626adad
user <user@192.168.219.104>
2020-11-27 23:57:50 +0900

    initial commit

--- src/Gugudan/Gugudan2.java
+++ src/Gugudan/Gugudan2.java
@@ -3,11 +3,13 @@ package Gugudan;
 public class Gugudan2 {
     public static void main(String[] args) {
         int[] result = new int[9];
-        for(int i=0; i <= result.length; i++) {
+        	for(int i=0; i < result.length; i++) {
+        		result[i] = 2 * (i+1);
             // 계산 결과를 배열에 저장
         }
 
-        for(int i=0; i <= result.length; i++) {
+        	for(int i=0; i <= result.length; i++) {
+        		System.out.println(result[i]);
            // 계산 결과를 화면에 출력
         }
     }
